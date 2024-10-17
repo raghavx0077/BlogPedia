c96439a2b2b95d502f8454e3a4c9cdc8ffeadb36
raghavx0077 <kapoorraghav832@gmail.com>
2024-10-18 01:13:47 +0530

    Blogging website

--- Blog-main/blog-express/database/Mongodb.js
+++ Blog-main/blog-express/database/Mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async() => {
     mongoose.set('strictQuery', true);
-    await mongoose.connect('mongodb://localhost:27017/Blogpedia');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('server is connected to the database...');
 }
 
